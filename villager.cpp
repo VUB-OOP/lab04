2d22c378ad3112057e84244c07f72c8020de7846
Krunoslav Husak <khusak@vub.hr>
2024-10-26 21:10:40 +0000

    Defaultni konstruktor koji postavlja početne vrijednosti

--- villager.cpp
+++ villager.cpp
@@ -1,1 +1,8 @@
 #include "villager.h"
+
+// Defaultni konstruktor koji postavlja podatkovne članove na početne (maksimalne) vrijednosti:
+Villager::Villager() : 
+  health(VILLAGER_MAX_HEALTH),
+  attack(VILLAGER_MAX_ATTACK),
+  speed(VILLAGER_MAX_SPEED) {
+}
--- villager.h
+++ villager.h
@@ -1,8 +1,14 @@
+#define VILLAGER_MAX_HEALTH 25
+#define VILLAGER_MAX_ATTACK 3
+#define VILLAGER_MAX_SPEED 0.8
+
 class Villager {
 public:
+  // Defaultni konstruktor koji postavlja podatkovne članove na početne (maksimalne) vrijednosti: health - 25, attack - 3, speed - 0.8.
+  Villager();
 
 private:
   int health;
   int attack;
-  int speed;
+  double speed;
 };
