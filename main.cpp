e078913c398fc47c6988c72234ee082dddefcf13
Krunoslav Husak <khusak@vub.hr>
2024-10-26 21:21:38 +0000

    Testiranje implementiranog u mainu

--- main.cpp
+++ main.cpp
@@ -2,5 +2,12 @@
 #include "villager.h"
 
 int main() {
-  // Villager v1;
+  Villager v1;
+  v1.info(); // Ispisuje: Villager (25, 3, 0.8)
+
+  Villager v2(10);
+  v2.info(); // Ispisuje: Villager (10, 3, 0.8)
+
+  Villager v3 = v2; // Može i: Villager v3(v2);
+  v3.info(); // Ispisuje: Villager (25, 3, 0.8)
 }
